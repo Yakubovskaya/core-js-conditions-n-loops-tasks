ff18aa72d407c282916ab2b74ecb2832d4b60428
Marina Yakubovskaya <maribars93@yandex.ru>
2025-11-07 00:38:52 +0300

    feat: implement convertToRomanNumerals function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -163,8 +163,33 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  let result = '';
+
+  let number = num;
+
+  while (number >= 10) {
+    result += 'X';
+    number -= 10;
+  }
+
+  if (number === 9) {
+    result += 'IX';
+    number -= 9;
+  } else if (number >= 5) {
+    result += 'V';
+    number -= 5;
+  } else if (number === 4) {
+    result += 'IV';
+    number -= 4;
+  }
+
+  while (number >= 1) {
+    result += 'I';
+    number -= 1;
+  }
+
+  return result;
 }
 
 /**
